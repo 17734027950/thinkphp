38a9d84d711390a0ca4e659aeaa68d0ba753cb67
17734027950 <17734027950@qq.com>
2018-09-19 09:41:51 +0800

    增加null2string

--- application/common.php
+++ application/common.php
@@ -10,3 +10,25 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
+
+/**
+ * null转空字符串
+ * [null2string description]
+ * @author [YangChi] <[18210427950@qq.com]>
+ * @DateTime [2018-09-19T09:37:21+0800]
+ * @version  [version]
+ * @param    array                      $arr [description]
+ * @return   [type]                          [description]
+ */
+function null2string(array $arr = array())
+{
+    foreach ($arr as $k=>$v){
+        if(is_null($v)) {
+            $arr [$k] = '';
+        }
+        if(is_array($v)) {
+            $arr [$k] = nulltostr($v);
+        }
+    }
+    return $arr;
+}
